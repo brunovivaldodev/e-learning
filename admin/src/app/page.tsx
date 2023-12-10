ebee12b2a5660549404ed8e2679460b84568a1fc
Bruno Vivaldo <brunolumeca@live.com>
2023-12-10 22:29:14 +0100

    feat:Details page base

--- admin/src/app/page.tsx
+++ admin/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Bell, MagnifyingGlass, Star } from "@phosphor-icons/react/dist/ssr";
+import { Star } from "@phosphor-icons/react/dist/ssr";
 import { User } from "@nextui-org/react";
 import { Card } from "@/components/card";
 import { AreaChart } from "@/components/area-chart";
@@ -7,6 +7,7 @@ import { LineChart } from "@/components/line-chart";
 import { LineChartTest } from "@/components/line-chart-test";
 import { ProgressBar } from "@tremor/react";
 import { Header } from "@/components/header";
+import { Evaluation } from "@/components/evaluation";
 
 export default function Home() {
   return (
@@ -107,109 +108,7 @@ export default function Home() {
             </div>
           </div>
           <div className="flex gap-6">
-            <div className="flex flex-col w-96 bg-white">
-              <div className="h-12 flex items-center p-2 justify-between ">
-                <h4>Avaliação geral dos curso</h4>
-                <select className="bg-white ">
-                  <option value="">Hoje</option>
-                  <option value="">Ontem</option>
-                </select>
-              </div>
-              <div className="border"></div>
-              <div className="flex p-4 gap-4">
-                <div className="flex  flex-col bg-red-100 p-4 items-center justify-center h-36 gap-2">
-                  <h2 className="text-2xl font-medium">4.6</h2>
-                  <div className="flex">
-                    <Star />
-                    <Star />
-                    <Star />
-                    <Star />
-                    <Star />
-                  </div>
-                  <p>Avaliação geral</p>
-                </div>
-                <div className="flex-1">
-                  <LineChartTest />
-                </div>
-              </div>
-              <div className="p-4">
-                <div className="flex-1">
-                  <div className="flex justify-center gap-2">
-                    <div className="flex">
-                      <Star size={12} color="#e3cc36" weight="fill" />
-                      <Star size={12} color="#e3cc36" weight="fill" />
-                      <Star size={12} color="#e3cc36" weight="fill" />
-                      <Star size={12} color="#e3cc36" weight="fill" />
-                      <Star size={12} color="#e3cc36" weight="fill" />
-                    </div>
-                    <div className="flex flex-1 gap-2">
-                      <ProgressBar value={56} color="yellow" />
-                      <p>56%</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="flex-1 ">
-                  <div className="flex justify-center gap-2">
-                    <div className="flex">
-                      <Star size={12} color="#e3cc36" weight="fill" />
-                      <Star size={12} color="#e3cc36" weight="fill" />
-                      <Star size={12} color="#e3cc36" weight="fill" />
-                      <Star size={12} color="#e3cc36" weight="fill" />
-                      <Star size={12} color="#e3cc36" />
-                    </div>
-                    <div className="flex flex-1 gap-2">
-                      <ProgressBar value={37} color="yellow" />
-                      <p>37%</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="flex-1 ">
-                  <div className="flex justify-center gap-2">
-                    <div className="flex">
-                      <Star size={12} color="#e3cc36" weight="fill" />
-                      <Star size={12} color="#e3cc36" weight="fill" />
-                      <Star size={12} color="#e3cc36" weight="fill" />
-                      <Star size={12} color="#e3cc36" />
-                      <Star size={12} color="#e3cc36" />
-                    </div>
-                    <div className="flex flex-1 gap-1">
-                      <ProgressBar value={8} color="yellow" />
-                      <p>8%</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="flex-1">
-                  <div className="flex justify-center gap-2">
-                    <div className="flex">
-                      <Star size={12} color="#e3cc36" weight="fill" />
-                      <Star size={12} color="#e3cc36" weight="fill" />
-                      <Star size={12} color="#e3cc36" />
-                      <Star size={12} color="#e3cc36" />
-                      <Star size={12} color="#e3cc36" />
-                    </div>
-                    <div className="flex flex-1 gap-2">
-                      <ProgressBar value={1} color="yellow" />
-                      <p>1%</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="flex-1 ">
-                  <div className="flex justify-center gap-2">
-                    <div className="flex">
-                      <Star size={12} color="#e3cc36" weight="fill" />
-                      <Star size={12} color="#e3cc36" />
-                      <Star size={12} color="#e3cc36" />
-                      <Star size={12} color="#e3cc36" />
-                      <Star size={12} color="#e3cc36" />
-                    </div>
-                    <div className="flex flex-1 gap-2">
-                      <ProgressBar value={6} color="yellow" />
-                      <p>6%</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <Evaluation />
 
             <div className="flex flex-1 flex-col  bg-white">
               <div className="h-12 flex items-center p-2 justify-between ">
