b3d8bef65fbbb01e12d64bd42241fd66092d3e5a
Bruno Vivaldo <brunolumeca@live.com>
2024-01-10 19:24:05 +0100

    feat: instructor app

---
+++ instructor/src/app/page.tsx
@@ -0,0 +1,126 @@
+import { User } from "@nextui-org/react";
+import { Card } from "@/components/card";
+import { AreaChart } from "@/components/area-chart";
+import { BarChart } from "@/components/bar-chart";
+import { LineChart } from "@/components/line-chart";
+import { Header } from "@/components/header";
+import { Evaluation } from "@/components/evaluation";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col space-y-8">
+      <Header page="Painel" />
+      <div className="mx-28 space-y-8">
+        <section className="grid grid-cols-4 gap-4">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </section>
+        <section className="flex flex-col gap-8">
+          <div className="flex gap-6">
+            <div className="flex flex-col w-80 bg-white">
+              <div className="h-12 flex items-center p-2 justify-between ">
+                <h4>Vista de perfil</h4>
+                <select className="bg-white ">
+                  <option value="">Hoje</option>
+                  <option value="">Ontem</option>
+                </select>
+              </div>
+              <div className="border"></div>
+              <div className="flex p-2">
+                <User
+                  name=""
+                  avatarProps={{
+                    src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
+                  }}
+                />
+                <div>
+                  <p>
+                    <strong>Kevin</strong> comentou no seu curso{" "}
+                    <strong>React na prática</strong> {"  "}
+                    "Muito bom esse curso gostei"
+                  </p>
+                </div>
+              </div>
+              <div className="flex p-2">
+                <User
+                  name=""
+                  avatarProps={{
+                    src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
+                  }}
+                />
+                <div>
+                  <p>
+                    <strong>Kevin</strong> comentou no seu curso{" "}
+                    <strong>React na prática</strong> {"  "}
+                    "Muito bom esse curso gostei"
+                  </p>
+                </div>
+              </div>
+              <div className="flex p-2">
+                <User
+                  name=""
+                  avatarProps={{
+                    src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
+                  }}
+                />
+                <div>
+                  <p>
+                    <strong>Kevin</strong> comentou no seu curso{" "}
+                    <strong>React na prática</strong> {"  "}
+                    "Muito bom esse curso gostei"
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col w-96 bg-white">
+              <div className="h-12 flex items-center p-2 justify-between ">
+                <h4>Receita</h4>
+                <select className="bg-white ">
+                  <option value="">Hoje</option>
+                  <option value="">Ontem</option>
+                </select>
+              </div>
+              <div className="border"></div>
+
+              <AreaChart />
+            </div>
+
+            <div className="flex flex-col w-64 bg-white">
+              <div className="h-12 flex items-center p-2 justify-between ">
+                <h4>Vista de</h4>
+                <select className="bg-white ">
+                  <option value="">Hoje</option>
+                  <option value="">Ontem</option>
+                </select>
+              </div>
+              <div className="border"></div>
+              <BarChart />
+            </div>
+          </div>
+          <div className="flex gap-6">
+            <Evaluation />
+
+            <div className="flex flex-1 flex-col  bg-white">
+              <div className="h-12 flex items-center p-2 justify-between ">
+                <h4>Visão geral do curso</h4>
+                <select className="bg-white ">
+                  <option value="">Esta semana</option>
+                  <option value="">Ontem</option>
+                </select>
+              </div>
+              <div className="border"></div>
+              <LineChart />
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+}
